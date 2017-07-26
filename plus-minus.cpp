28e0497f487a5a0bf94cd35cd84206a3f289ada2
LI Haocheng <hcli@cse.cuhk.edu.hk>
2017-07-26 14:51:38 +0800

    Update plus-minus.cpp

--- plus-minus.cpp
+++ plus-minus.cpp
@@ -1,12 +1,34 @@
 #include <cmath>
 #include <cstdio>
-#include <vector>
+#include <iomanip>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    size_t n = 0;
+    cin >> n;
+    unsigned p = 0;
+    unsigned m = 0;
+    unsigned z = 0;
+    int a = 0;
+    for (size_t i = 0; i != n; ++i) {
+        cin >> a;
+        if (a > 0) {
+            ++p;
+        } else if (a < 0) {
+            ++m;
+        } else {
+            ++z;
+        }
+    }
+    cout << fixed << setprecision(6);
+    cout << p / (double)n << endl;
+    cout << m / (double)n << endl;
+    cout << z / (double)n << endl;
     return 0;
 }
