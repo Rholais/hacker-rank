23683d02aa546a30c1ab532db9720c5149ff9a91
Rholais <lhch95@163.com>
2017-07-27 21:14:46 +0800

    Update save-the-prisoner.cpp

--- implementation/save-the-prisoner.cpp
+++ implementation/save-the-prisoner.cpp
@@ -7,6 +7,15 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    size_t t = 0;
+    cin >> t;
+    size_t n = 0;
+    size_t m = 0;
+    size_t s = 0;
+    for (size_t i = 0; i != t; ++i) {
+        cin >> n >> m >> s;
+        cout << (m + s - 2) % n + 1 << endl;
+    }
     return 0;
 }
