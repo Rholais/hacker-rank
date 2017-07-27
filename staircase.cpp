e282b72f987d8a61f7ef874580ce14dc04afb947
LI Haocheng <hcli@cse.cuhk.edu.hk>
2017-07-27 13:42:34 +0800

    Update staircase.cpp

--- staircase.cpp
+++ staircase.cpp
@@ -7,6 +7,18 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    size_t n = 0;
+    cin >> n;
+    for (size_t i = 0; i != n; ++i) {
+        size_t j = 0;
+        for (; j != n - i - 1; ++j) {
+            cout << ' ';
+        }
+        for (; j != n; ++j) {
+            cout << '#';
+        }
+        cout << endl;
+    }
     return 0;
 }
