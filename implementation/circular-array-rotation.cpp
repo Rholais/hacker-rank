39c7126e4c8676f80b337f28e7e16ae8ee324d61
Rholais <lhch95@163.com>
2017-07-27 22:25:08 +0800

    Update circular-array-rotation.cpp

--- implementation/circular-array-rotation.cpp
+++ implementation/circular-array-rotation.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    size_t n = 0;
+    size_t k = 0;
+    size_t q = 0;
+    cin >> n >> k >> q;
+    vector<int> a;
+    int b = 0;
+    for (size_t i = 0; i != n; ++i) {
+        cin >> b;
+        a.push_back(b);
+    }
+    int x = 0;
+    for (size_t i = 0; i != q; ++i) {
+        cin >> x;
+        cout << a[(x + k * (n - 1)) % n] << endl;
+    }
     return 0;
 }
